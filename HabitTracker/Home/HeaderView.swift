993c68d4e5467ec98b63bcc80a9745a3202f488a
Ritika Hotwani <ritikahotwani24@gmail.com>
2025-12-21 23:47:59 +0530

    App header final

--- HabitTracker/Home/HeaderView.swift
+++ HabitTracker/Home/HeaderView.swift
@@ -7,19 +7,23 @@
 import SwiftUI
 
 struct HeaderView: View {
-    @EnvironmentObject var vm: HabitTrackerViewModel
+//    @EnvironmentObject var vm: HabitTrackerViewModel
     var body: some View {
         HStack {
             VStack(alignment: .leading, spacing: 4) {
-                if let user = vm.currentUser {
-                    Text("Hello,")
-                        .font(.system(size: 30, weight: .semibold, design: .rounded))
-                        .foregroundStyle(AppGradient.purple)
-                    
-                    Text("\(user.userName ?? "")")
-                        .font(.system(size: 28, weight: .medium, design: .rounded))
-                        .foregroundStyle(.black)
-                }
+                Text("𝓣𝓲𝓷𝔂 𝓦𝓲𝓷𝓼")
+                    .font(.system(size: 36))
+                    .foregroundStyle(AppGradient.purple)
+
+//                if let user = vm.currentUser {
+//                    Text("Hello,")
+//                        .font(.system(size: 30, weight: .semibold, design: .rounded))
+//                        .foregroundStyle(AppGradient.purple)
+//                    
+//                    Text("\(user.userName ?? "")")
+//                        .font(.system(size: 28, weight: .medium, design: .rounded))
+//                        .foregroundStyle(.black)
+//                }
             }
             Spacer()
 
